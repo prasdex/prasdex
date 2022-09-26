85e736112e8ff3216ca932c27996386acfec6b92
prasdex <114388692+prasdex@users.noreply.github.com>
2022-09-26 14:46:26 +0700

    Add files via upload
    dex

---
+++ prasdex.php
@@ -0,0 +1,11 @@
+<!-- static resource -->
+<link rel="stylesheet" href="styles.css">
+
+<!-- Dynamic styles -->
+<style type="text/css">
+
+  body { color: <?php echo $body_color; ?>; }
+  h1   { font-size: <?php echo $fontsize."px"; ?>; }
+  p    { color: <?php echo $paragraph_color; ?>; }
+
+</style>
